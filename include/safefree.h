8f8b46e436ba77e2509149cf9b1eadfbc26f3828
Aibar <japo0n@mail.ru>
2025-01-13 21:59:24 +0500

    safefree.h update

--- include/safefree.h
+++ include/safefree.h
@@ -1,12 +1,12 @@
 #ifndef SAFEFREE_H
 #define SAFEFREE_H
 
-#define SAFE_FREE(p)        \
-	do {                    \
-        if((p) != NULL) {   \
-		free(p);            \
-		(p) = NULL;         \
-        }                   \
+#define SAFE_FREE(p)       \
+	do {                   \
+		if ((p) != NULL) { \
+			free(p);       \
+			(p) = NULL;    \
+		}                  \
 	} while (0)
 
 #endif
